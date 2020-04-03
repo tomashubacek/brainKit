a3dd1dd37de7dd75344c19a7f8c9d03028c7aee9
Ondrej Hanak <oh@ondrejhanak.cz>
2020-04-03 17:01:11 +0200

    CGFloat extension

---
+++ Sources/Extensions/CGFloat+ubr.swift
@@ -0,0 +1,15 @@
+//
+//  CGFloat+ubr.swift
+//  BrainKit
+//
+//  Created by Ondřej Hanák on 03. 04. 2020.
+//  Copyright © 2020 Userbrain. All rights reserved.
+//
+
+import UIKit
+
+extension CGFloat {
+	public static var hairlineWidth: CGFloat {
+		1 / UIScreen.main.scale
+	}
+}
