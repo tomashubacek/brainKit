ffe3e8327470781773267f69fd706a4cc69511e8
Ondrej Hanak <oh@ondrejhanak.cz>
2020-05-04 19:35:19 +0200

    File size getter

---
+++ Sources/Foundation/Extensions/FileManager+Foundation.swift
@@ -0,0 +1,17 @@
+//
+//  FileManager+Foundation.swift
+//  BrainKit
+//
+//  Created by Ondřej Hanák on 04. 05. 2020.
+//  Copyright © 2020 Userbrain. All rights reserved.
+//
+
+import Foundation
+
+extension FileManager {
+	public func sizeOfFile(at url: URL) throws -> UInt64 {
+		let attr = try FileManager.default.attributesOfItem(atPath: url.path)
+		let size = attr[FileAttributeKey.size] as? UInt64 ?? 0
+		return size
+	}
+}
