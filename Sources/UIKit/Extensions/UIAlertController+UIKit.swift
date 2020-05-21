c00b7ace0a95b7477d1f7ec58225ef3c30cf579c
Ondrej Hanak <oh@ondrejhanak.cz>
2020-05-21 22:27:59 +0200

    UIAlertController extensions

---
+++ Sources/UIKit/Extensions/UIAlertController+UIKit.swift
@@ -0,0 +1,42 @@
+//
+//  UIAlertController+UIKit.swift
+//  UnitTests
+//
+//  Created by Ondřej Hanák on 21. 05. 2020.
+//  Copyright © 2020 Userbrain. All rights reserved.
+//
+
+import UIKit
+
+extension UIAlertController {
+	public static func makeAlert(title: String?, message: String?) -> UIAlertController {
+		return UIAlertController(title: title, message: message, preferredStyle: .alert)
+	}
+
+	@discardableResult
+	public func addAction(title: String, style: UIAlertAction.Style, handler: ((UIAlertAction) -> Void)?) -> UIAlertAction {
+		let action = UIAlertAction(title: title, style: style, handler: handler)
+		self.addAction(action)
+		return action
+	}
+
+	@discardableResult
+	public func addDefaultAction(title: String, handler: ((UIAlertAction) -> Void)?) -> UIAlertAction {
+		return self.addAction(title: title, style: .default, handler: handler)
+	}
+
+	@discardableResult
+	public func addDestructiveAction(title: String, handler: ((UIAlertAction) -> Void)?) -> UIAlertAction {
+		return self.addAction(title: title, style: .destructive, handler: handler)
+	}
+
+	@discardableResult
+	public func addCancelAction(title: String, handler: ((UIAlertAction) -> Void)?) -> UIAlertAction {
+		return self.addAction(title: title, style: .cancel, handler: handler)
+	}
+
+	@discardableResult
+	public func addCancelAction(title: String) -> UIAlertAction {
+		return self.addCancelAction(title: title, handler: nil)
+	}
+}
