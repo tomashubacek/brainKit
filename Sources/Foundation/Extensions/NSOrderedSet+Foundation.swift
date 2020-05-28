2effc9dbf18dba9069b032b92b688b4f2ee53bd4
Ondrej Hanak <oh@ondrejhanak.cz>
2020-05-28 14:21:03 +0200

    isEmpty test method

---
+++ Sources/Foundation/Extensions/NSOrderedSet+Foundation.swift
@@ -0,0 +1,15 @@
+//
+//  NSOrderedSet+Foundation.swift
+//  BrainKit
+//
+//  Created by Ondřej Hanák on 28. 05. 2020.
+//  Copyright © 2020 Userbrain. All rights reserved.
+//
+
+import Foundation
+
+extension NSOrderedSet {
+	public var isEmpty: Bool {
+		return self.count == 0
+	}
+}
---
+++ UnitTests/NSOrderedSetTests.swift
@@ -0,0 +1,20 @@
+//
+//  NSOrderedSetTests.swift
+//  UnitTests
+//
+//  Created by Ondřej Hanák on 28. 05. 2020.
+//  Copyright © 2020 Userbrain. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+final class NSOrderedSetTestsTests: XCTestCase {
+	func test_isEmpty() {
+		let empty = NSOrderedSet()
+		XCTAssertTrue(empty.isEmpty)
+
+		let nonEmpty = NSOrderedSet(array: [1])
+		XCTAssertFalse(nonEmpty.isEmpty)
+	}
+}
