4fd76feb75c7922d4b0ac4327342f0a14fccb509
Ondrej Hanak <oh@ondrejhanak.cz>
2020-09-17 13:39:45 +0200

    Font scaling method updated

--- Sources/UIKit/Extensions/UIFont+UIKit.swift
+++ Sources/UIKit/Extensions/UIFont+UIKit.swift
@@ -8,12 +8,22 @@
 
 import UIKit
 
-@available(iOS 11.0, *)
 extension UIFont {
 	public static func preferredFont(forTextStyle style: TextStyle, weight: Weight) -> UIFont {
-		let desc = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style)
-		let font = UIFont.systemFont(ofSize: desc.pointSize, weight: weight)
-		let metrics = UIFontMetrics(forTextStyle: style)
-		return metrics.scaledFont(for: font)
+		let preferred = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style)
+		let size = preferred.pointSize
+		let font = UIFont.systemFont(ofSize: size, weight: weight)
+		return font
+	}
+
+	@available(iOS 13.0, *)
+	public static func preferredFont(forTextStyle style: TextStyle, weight: Weight, design: UIFontDescriptor.SystemDesign = .default) -> UIFont {
+		let preferred = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style)
+		let size = preferred.pointSize
+		var font = UIFont.systemFont(ofSize: size, weight: weight)
+		if let desc = font.fontDescriptor.withDesign(design) {
+			font = UIFont(descriptor: desc, size: size)
+		}
+		return font
 	}
 }
