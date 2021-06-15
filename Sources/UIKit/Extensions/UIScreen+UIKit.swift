edfbf49bb8fea84f255fffa287aa3d2f9fff55f4
Ondrej Hanak <oh@ondrejhanak.cz>
2021-06-15 10:28:40 +0200

    UIScreen.correctBounds()

---
+++ Sources/UIKit/Extensions/UIScreen+UIKit.swift
@@ -0,0 +1,18 @@
+//
+//  UIScreen+UIKit.swift
+//  BrainKit
+//
+//  Created by Ondřej Hanák on 15. 06. 2021.
+//  Copyright © 2021 Userbrain. All rights reserved.
+//
+
+import UIKit
+
+extension UIScreen {
+	public static func correctBounds() -> CGRect {
+		if let topMostViewController = UIApplication.shared.topMostViewController {
+			return topMostViewController.view.bounds
+		}
+		return self.main.bounds
+	}
+}
