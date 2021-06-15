7511df98292634c211dcbed96be33142e8afa6bd
Ondrej Hanak <oh@ondrejhanak.cz>
2021-06-15 11:25:02 +0200

    FloatingPoint.map limitToBounds param

--- Sources/Foundation/Extensions/FloatingPoint+Foundation.swift
+++ Sources/Foundation/Extensions/FloatingPoint+Foundation.swift
@@ -9,11 +9,22 @@
 import Foundation
 
 extension FloatingPoint {
-	public func map(from: (Self, Self), to: (Self, Self)) -> Self {
+	public func map(from: (Self, Self), to: (Self, Self), limitToBounds: Bool = false) -> Self {
 		let value = self
 		let fromSize = from.1 - from.0
 		guard fromSize != 0 else { return Self.nan }
 		let toSize = to.1 - to.0
-		return to.0 + toSize * (value - from.0) / fromSize
+		let result = to.0 + toSize * (value - from.0) / fromSize
+		if limitToBounds {
+			let min = min(to.0, to.1)
+			if result < min {
+				return min
+			}
+			let max = max(to.0, to.1)
+			if result > max {
+				return max
+			}
+		}
+		return result
 	}
 }
--- UnitTests/FloatingPointExtensionsTests.swift
+++ UnitTests/FloatingPointExtensionsTests.swift
@@ -29,6 +29,18 @@ final class FloatingPointExtensionsTests: XCTestCase {
 		XCTAssertEqual(x, 50)
 	}
 
+	func test_OutOfRangeLimitBelow() {
+		let num = 5.0
+		let x = num.map(from: (6,10), to: (60,100), limitToBounds: true)
+		XCTAssertEqual(x, 60)
+	}
+
+	func test_OutOfRangeLimitAbove() {
+		let num = 11.0
+		let x = num.map(from: (6,10), to: (60,100), limitToBounds: true)
+		XCTAssertEqual(x, 100)
+	}
+
 	func test_InRange() {
 		let num = 2.0
 		let x = num.map(from: (1,3), to: (10,12))
@@ -41,9 +53,27 @@ final class FloatingPointExtensionsTests: XCTestCase {
 		XCTAssertEqual(x, 3)
 	}
 
-	func test_ReversedOutIfRange() {
+	func test_ReversedOutOfRange() {
 		let num = -1.0
 		let x = num.map(from: (1,4), to: (4,1))
 		XCTAssertEqual(x, 6)
+	}
+
+	func test_ReversedOutOfRangeLimitBelow() {
+		let num = 5.0
+		let x = num.map(from: (1,4), to: (4,1), limitToBounds: true)
+		XCTAssertEqual(x, 1)
+	}
+
+	func test_ReversedOutOfRangeLimitAbove() {
+		let num = -1.0
+		let x = num.map(from: (1,4), to: (4,1), limitToBounds: true)
+		XCTAssertEqual(x, 4)
+	}
+
+	func test_EmptyRange() {
+		let num = 1.0
+		let x = num.map(from: (1,1), to: (1,2))
+		XCTAssertTrue(x.isNaN)
 	}
 }
