81ca398d7e565bb8d3e32d4685c144ad216deff7
Ondrej Hanak <oh@ondrejhanak.cz>
2021-10-15 09:45:31 +0200

    UIColor extensions

--- Sources/UIKit/Extensions/UIColor+UIKit.swift
+++ Sources/UIKit/Extensions/UIColor+UIKit.swift
@@ -15,4 +15,36 @@ extension UIColor {
 			rendererContext.fill(CGRect(origin: .zero, size: size))
 		}
 	}
+
+	public var rgbComponents: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+		var r: CGFloat = 0
+		var g: CGFloat = 0
+		var b: CGFloat = 0
+		var a: CGFloat = 0
+		if self.getRed(&r, green: &g, blue: &b, alpha: &a) {
+			return (r, g, b, a)
+		}
+		return (0, 0, 0, 0)
+	}
+
+	public var hsbComponents: (hue: CGFloat, saturation: CGFloat, brightness: CGFloat, alpha: CGFloat) {
+		var hue: CGFloat = 0
+		var saturation: CGFloat = 0
+		var brightness: CGFloat = 0
+		var alpha: CGFloat = 0
+		if self.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha) {
+			return (hue, saturation, brightness, alpha)
+		}
+		return (0, 0, 0, 0)
+	}
+
+	public var htmlRGBHexColor: String {
+		let components = self.rgbComponents
+		return String(format: "#%02x%02x%02x", Int(components.red * 255), Int(components.green * 255), Int(components.blue * 255))
+	}
+
+	public var htmlRGBaHexColor: String {
+		let components = self.rgbComponents
+		return String(format: "#%02x%02x%02x%02x", Int(components.red * 255), Int(components.green * 255), Int(components.blue * 255), Int(components.alpha * 255) )
+	}
 }
