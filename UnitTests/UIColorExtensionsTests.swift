c3128ca6345225baa410a0f1239bcbc59bb55cea
Ondrej Hanak <oh@ondrejhanak.cz>
2021-10-15 13:33:34 +0200

    UIColor tests

--- UnitTests/UIColorExtensionsTests.swift
+++ UnitTests/UIColorExtensionsTests.swift
@@ -15,7 +15,7 @@ final class UIColorExtensionsTests: XCTestCase {
 		let r = 50.0
 		let g = 100.0
 		let b = 150.0
-		let a = 255.0 // othet than 255 alters RGB values
+		let a = 255.0 // other than 255 alters RGB values
 		let color = UIColor(red: CGFloat(r / 255), green: CGFloat(g / 255), blue: CGFloat(b / 255), alpha: CGFloat(a / 255))
 		let size = CGSize(width: 10, height: 20)
 
@@ -35,5 +35,55 @@ final class UIColorExtensionsTests: XCTestCase {
 		XCTAssertEqual(g, imageG)
 		XCTAssertEqual(b, imageB)
 		XCTAssertEqual(a, imageA)
+	}
+
+	func test_hsbComponents() {
+		let h = 0.125
+		let s = 0.25
+		let b = 0.5
+		let a = 1 // other than 1 alters RGB values
+		let color = UIColor(hue: CGFloat(h), saturation: CGFloat(s), brightness: CGFloat(b), alpha: CGFloat(a))
+
+		let components = color.hsbComponents
+		XCTAssertEqual(components.hue, CGFloat(h))
+		XCTAssertEqual(components.saturation, CGFloat(s))
+		XCTAssertEqual(components.brightness, CGFloat(b))
+		XCTAssertEqual(components.alpha, CGFloat(a))
+	}
+
+	func test_rgbaComponents() {
+		let r = 0.125
+		let g = 0.25
+		let b = 0.5
+		let a = 1 // other than 1 alters RGB values
+		let color = UIColor(red: CGFloat(r), green: CGFloat(g), blue: CGFloat(b), alpha: CGFloat(a))
+
+		let components = color.rgbComponents
+		XCTAssertEqual(components.red, CGFloat(r))
+		XCTAssertEqual(components.green, CGFloat(g))
+		XCTAssertEqual(components.blue, CGFloat(b))
+		XCTAssertEqual(components.alpha, CGFloat(a))
+	}
+
+	func test_rgbHexColor() {
+		let r = 17.0
+		let g = 34.0
+		let b = 51.0
+		let a = 255.0 // other than 255 alters RGB values
+		let color = UIColor(red: CGFloat(r / 255.0), green: CGFloat(g / 255.0), blue: CGFloat(b / 255.0), alpha: CGFloat(a / 255.0))
+
+		let hexaColor = color.htmlRGBHexColor
+		XCTAssertEqual(hexaColor, "#112233")
+	}
+
+	func test_rgbaHexColor() {
+		let r = 17.0
+		let g = 34.0
+		let b = 51.0
+		let a = 255.0 // other than 255 alters RGB values
+		let color = UIColor(red: CGFloat(r / 255.0), green: CGFloat(g / 255.0), blue: CGFloat(b / 255.0), alpha: CGFloat(a / 255.0))
+
+		let hexaColor = color.htmlRGBaHexColor
+		XCTAssertEqual(hexaColor, "#112233ff")
 	}
 }
