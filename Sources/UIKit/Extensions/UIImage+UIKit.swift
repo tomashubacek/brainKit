19037282ffa25fe8be8e83f77cbd1b516bc180a9
Ondrej Hanak <oh@ondrejhanak.cz>
2021-11-12 15:28:41 +0100

    UIImage darken/lighten methods

--- Sources/UIKit/Extensions/UIImage+UIKit.swift
+++ Sources/UIKit/Extensions/UIImage+UIKit.swift
@@ -8,6 +8,54 @@
 
 import UIKit
 
+// based on https://stackoverflow.com/a/58981946/1548913
+extension UIImage {
+	private enum BlendMode {
+		case multiply // This results in colors that are at least as dark as either of the two contributing sample colors
+		case screen // This results in colors that are at least as light as either of the two contributing sample colors
+	}
+
+	// A level of zero yeilds the original image, a level of 1 results in black
+	public func darken(level: CGFloat = 0.5) -> UIImage? {
+		return blend(mode: .multiply, level: level)
+	}
+
+	// A level of zero yeilds the original image, a level of 1 results in white
+	public func lighten(level: CGFloat = 0.5) -> UIImage? {
+		return blend(mode: .screen, level: level)
+	}
+
+	private func blend(mode: BlendMode, level: CGFloat) -> UIImage? {
+		let context = CIContext(options: nil)
+		var level = max(0, min(level, 1)) // Ensure <0;1> closed interval
+		let filterName: String
+		switch mode {
+		case .multiply:
+			level = abs(level - 1.0)
+			filterName = "CIMultiplyBlendMode"
+			case .screen:
+			filterName = "CIScreenBlendMode"
+		}
+
+		let blender = CIFilter(name: filterName)!
+		let backgroundColor = CIColor(color: UIColor(white: level, alpha: 1))
+
+		guard let inputImage = CIImage(image: self) else { return nil }
+		blender.setValue(inputImage, forKey: kCIInputImageKey)
+
+		guard let backgroundImageGenerator = CIFilter(name: "CIConstantColorGenerator") else { return nil }
+		backgroundImageGenerator.setValue(backgroundColor, forKey: kCIInputColorKey)
+		guard let backgroundImage = backgroundImageGenerator.outputImage?.cropped(to: CGRect(origin: CGPoint.zero, size: self.size)) else { return nil }
+		blender.setValue(backgroundImage, forKey: kCIInputBackgroundImageKey)
+
+		guard let blendedImage = blender.outputImage else { return nil }
+
+		guard let cgImage = context.createCGImage(blendedImage, from: blendedImage.extent) else { return nil }
+		return UIImage(cgImage: cgImage)
+	}
+}
+
+
 extension UIImage {
 	public func resized(toFit newSize: CGSize, upscale: Bool) -> UIImage {
 		// original metrics
